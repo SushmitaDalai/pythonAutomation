dce41e8871ec2e9fab29c2d00e13420a791c02e5
SushmitaDalai <dalaisushmita20@gmail.com>
2019-05-20 14:25:56 +0530

    publish job completion to SNS Topic

--- videoanalyzer/videolyzer/handler.py
+++ videoanalyzer/videolyzer/handler.py
@@ -1,5 +1,6 @@
 import urllib
 import boto3
+import os
 
 def start_label_detection(bucket, key):
     rekognition_client = boto3.client('rekognition')
@@ -9,8 +10,14 @@ def start_label_detection(bucket, key):
                 'Bucket': bucket,
                 'Name': key
             }
-        })
-
+        },
+        NotificationChannel={
+            'SNSTopicArn': os.environ["RECOGNITION_SNS_TOPIC_ARN"],
+            'RoleArn': os.environ["RECOGNITION_ROLE_ARN"]
+        }
+        )
+    print (os.environ["RECOGNITION_SNS_TOPIC_ARN"])
+    print (os.environ["RECOGNITION_ROLE_ARN"])
     print(response)
 
     return
@@ -18,4 +25,8 @@ def start_label_detection(bucket, key):
 def start_processing_video(event,context):
     for record in event['Records']:
         start_label_detection(record['s3']['bucket']['name'],urllib.parse.unquote_plus(record['s3']['object']['key']))
+    return
+
+def handle_label_detection(event,context):
+    print(event)
     return
